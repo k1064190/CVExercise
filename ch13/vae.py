34bf6d02a0d521df5c1ba850248ba89260dcce0c
gagasbandas <kjjcds@gmail.com>
2023-09-06 01:46:33 +0900

    Ch13 Vae 구현
    MNIST를 위한 Vae를 구현했다.
    Loss function을 직접 정의하는 부분이 제일 어려웠다.
    pytorch에서 2d image에 cross entropy 적용이 쉽지않음

---
+++ ch13/vae.py
@@ -0,0 +1,93 @@
+import cv2 as cv
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from tqdm import tqdm
+
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = torch.randn_like(z_mean)  # z_mean.shape = (batch_size, zdim)
+    return z_mean + torch.exp(z_log_var / 2) * epsilon
+
+class Encoder(nn.Module):
+    def __init__(self, in_channel=1, zdim=32, size=28):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channel, 32, kernel_size=3, stride=1, padding=1, bias=False),  # 32x28x28
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=False),  # 64x14x14
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=False),  # 128x7x7
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False),  # 256x7x7 # 256xsize/4xsize/4
+            nn.BatchNorm2d(256),
+            nn.ReLU(True)
+        )
+        self.flatten = nn.Flatten()
+        self.z_mean = nn.Linear(256 * (size // 4) * (size // 4), zdim)
+        self.z_log_var = nn.Linear(256 * (size // 4) * (size // 4), zdim)
+        self.encoder_output = torchvision.transforms.Lambda(sampling)
+
+    def forward(self, x):
+        # data를 이용해 z_mean, z_log_var을 학습하고, 이를 이용해 gaussian 분포에서 샘플링한 z를 반환
+        x = self.encoder(x)
+        x = self.flatten(x)
+        z_mean = self.z_mean(x)
+        z_log_var = self.z_log_var(x)
+        z = self.encoder_output([z_mean, z_log_var])
+        return z_mean, z_log_var, z
+
+
+class Decoder(nn.Module):
+    def __init__(self, out_channel=1, zdim=32, size=28):
+        super().__init__()
+
+        self.fc = nn.Linear(zdim, 256 * (size // 4) * (size // 4))
+        self.unflatten = nn.Unflatten(1, (256, size // 4, size // 4))
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=1, padding=1, bias=False),  # 128x7x7
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1, bias=False),  # 64x14x14
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1, bias=False),  # 32x28x28
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, out_channel, kernel_size=3, stride=1, padding=1, bias=False),  # 1x28x28
+        )
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = self.unflatten(x)
+        x = self.decoder(x)
+        x = torch.sigmoid(x)
+        return x
+
+
+class VaeLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, x_hat, z_mean, z_log_var):
+        nn.BCELoss()
+        reconstruction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction='none')    # (batch_size, 1, 28, 28)
+        reconstruction_loss = torch.sum(reconstruction_loss, dim=(1, 2, 3)) # (batch_size)
+        reconstruction_loss = torch.mean(reconstruction_loss)           # (1)
+        kl_divergence = 1 + z_log_var - z_mean.pow(2) - z_log_var.exp()    # (batch_size, zdim)
+        kl_divergence = -0.5 * torch.sum(kl_divergence, dim=1)            # (batch_size)
+        kl_divergence = torch.mean(kl_divergence)                         # (1)
+        total_loss = reconstruction_loss + kl_divergence
+        return total_loss
+
+
+
+
---
+++ ch13/vaeTest.py
@@ -0,0 +1,96 @@
+import cv2 as cv
+import numpy as np
+import torch
+import torchvision
+from tqdm import tqdm
+from vae import Encoder, Decoder, VaeLoss
+
+# Set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print("device:", device)
+torch.manual_seed(0)
+
+# Load MNIST dataset
+mnist_train = torchvision.datasets.MNIST(root='data', train=True, download=True, transform=torchvision.transforms.ToTensor())
+
+# Create a dataloader
+train_loader = torch.utils.data.DataLoader(mnist_train, batch_size=128, shuffle=True)
+
+zdim = 32
+
+
+# if model exists, load it
+import os
+if os.path.isfile('models/encoder_vae.pth') and os.path.isfile('models/decoder_vae.pth'):
+    encoder = Encoder(1, zdim, 28)
+    decoder = Decoder(1, zdim, 28)
+    encoder.to(device)
+    decoder.to(device)
+    encoder.load_state_dict(torch.load('models/encoder_vae.pth'))
+    decoder.load_state_dict(torch.load('models/decoder_vae.pth'))
+else:
+    encoder = Encoder(1, zdim, 28)
+    decoder = Decoder(1, zdim, 28)
+    encoder.to(device)
+    decoder.to(device)
+    params = [
+        {'params': encoder.parameters()},
+        {'params': decoder.parameters()}
+    ]
+
+    optimizer = torch.optim.Adam(params, lr=0.001)
+    # criterion = torch.nn.CrossEntropyLoss()
+    criterion = VaeLoss()
+
+    step_losses = []
+    epoch_losses = []
+
+    # Train the model
+    num_epochs = 10
+    for epoch in tqdm(range(num_epochs)):
+        encoder.train()
+        decoder.train()
+        epoch_loss = 0
+        for data, _ in tqdm(train_loader):
+            data = data.to(device)
+            # (batch_size, 1, 28, 28)
+            mu, logvar, encoded_data = encoder(data)    # z_mean, z_log_var, z
+            decoded_data = decoder(encoded_data)    # x_hat
+            loss = criterion(data, decoded_data, mu, logvar)
+            # loss = crossentropy(data, encoded_data) + 0.5 * torch.sum(torch.exp(logvar) + mu ** 2 - 1 - logvar)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+        epoch_loss /= len(train_loader)
+        epoch_losses.append(epoch_loss)
+        print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {epoch_loss:.4f}')
+    torch.save(encoder.state_dict(), 'models/encoder_vae.pth')
+    torch.save(decoder.state_dict(), 'models/decoder_vae.pth')
+    print("model saved")
+
+
+# Test the model with a sample
+import matplotlib.pyplot as plt
+
+mnist_test = torchvision.datasets.MNIST(root='data', train=False, download=True, transform=torchvision.transforms.ToTensor())
+test_loader = torch.utils.data.DataLoader(mnist_test, batch_size=16, shuffle=True)
+
+encoder.eval()
+decoder.eval()
+with torch.no_grad():
+    #   just 1 batch
+    data = next(iter(test_loader))[0]
+    data = data.to(device)
+    # use encoder to encode data
+    mu, logvar, encoded_data = encoder(data)
+    # use decoder to decode encoded data
+    decoded_data = decoder(encoded_data)
+    # show original data and decoded data
+    data = data.cpu().numpy()
+    decoded_data = decoded_data.cpu().numpy()
+    fig, axes = plt.subplots(2, 16, figsize=(16, 2)) # 2 rows, 16 columns
+    for i in range(16):
+        axes[0][i].imshow(data[i][0], cmap='gray')
+        axes[1][i].imshow(decoded_data[i][0], cmap='gray')
+    plt.show()
